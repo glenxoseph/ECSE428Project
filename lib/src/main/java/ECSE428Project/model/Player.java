607417aedae1d5e007dc018b769e6130dd0b8cf1
Antonin Guerre <antonin.guerre@mail.mcgill.ca>
2021-10-15 14:40:35 -0400

    Create new class

--- lib/src/main/java/ECSE428Project/model/Player.java
+++ lib/src/main/java/ECSE428Project/model/Player.java
@@ -1,10 +1,20 @@
 package ECSE428Project.model;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
 public class Player {
 
     private String id;
     private int numberOfCorrectAnswers;
     private int numberOfWrongAnswers;
-
 
 }
