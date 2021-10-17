bba6f5117fbde6015cf040f06369c6815e881b3b
Glen <jiayuan.xu@mail.mcgill.ca>
2021-10-17 15:46:23 -0400

    sample page

---
+++ 428-frontend/src/components/ECSE428.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="Question">
+    <h2>Question 1</h2>
+    <h3>Where is Trottier?</h3>
+    <table>
+      <tr>
+            <button>A. 845 Rue Sherbrooke O</button>
+      </tr>
+      <tr>
+            <button>B. 3630 Rue University</button>
+      </tr>
+      <tr>
+            <button>C. 859 Sherbrooke St W</button>
+      </tr>
+      <tr>
+              <input type="text" placeholder="Others...">
+      </tr>
+    </table>
+    <p>
+      <button>Submit</button>
+    </p>
+    <p>
+      <button>Go Back</button>
+    </p>
+    <p>
+      <span style="color:red">Error: Sample Error Message</span>
+    </p>
+  </div>
+</template>
+<script>
+</script>
+<style>
+  #Question {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+  tr {
+    text-align: left;
+  }
+  table {
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
--- 428-frontend/src/router/index.js
+++ 428-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import ECSE428 from '@/components/ECSE428'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'ECSE428',
+      component: ECSE428
     }
   ]
 })
