2facd4a94ed4a4ce091fbc70eb5328f7b1f9a3dc
MaxensDestine <38329417+maxensdestine@users.noreply.github.com>
2021-11-12 02:46:36 -0500

    Added service tests

--- lib/src/main/java/ECSE428Project/controller/MatchController.java
+++ lib/src/main/java/ECSE428Project/controller/MatchController.java
@@ -1,9 +1,51 @@
 package ECSE428Project.controller;
 
+import java.util.List;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import ECSE428Project.model.Match;
+import ECSE428Project.service.MatchService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class MatchController {
+
+	@Autowired
+	private MatchService matchService;
+
+	@GetMapping(path = { "/match/history", "/match/history/" })
+	public Set<Match> getAllMatches() throws ResponseStatusException {
+		return matchService.getAllMatch();
+	}
+
+	@GetMapping(path = { "/match/history/{email}", "/match/history/{email}/" })
+	public Set<Match> getAccountMatches(@PathVariable String email) throws ResponseStatusException {
+		Set<Match> matches = matchService.getMatchOfAccount(email);
+		if (matches == null) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"No account with the corresponding email can be found.");
+		}
+
+		return matches;
+	}
+
+	@PostMapping(path = { "/match/create", "/match/create/" })
+	public Match createMatch(@RequestBody List<String> emails) throws ResponseStatusException {
+		Match match = null;
+		try {
+			match = matchService.createMatch(emails);
+		} catch (IllegalArgumentException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+
+		return match;
+	}
 }
--- lib/src/main/java/ECSE428Project/model/Account.java
+++ lib/src/main/java/ECSE428Project/model/Account.java
@@ -1,106 +1,161 @@
 package ECSE428Project.model;
 
-
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table (name = "account")
+@Table(name = "account")
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class)
 public class Account {
 
-    //------------------------
-    // ATTRIBUTES
-    //------------------------
+	// ------------------------
+	// ATTRIBUTES
+	// ------------------------
 
-    @Id
-    private String email;
+	@Id
+	private String email;
 
-    private String name;
-    private String password;
-    private boolean isVerified;
-    private boolean isLoggedIn;
-    private int score;
-    private int level;
+	private String name;
+	private String password;
+	private boolean isVerified;
+	private boolean isLoggedIn;
+	private int score;
+	private int level;
 
+	// ------------------------
+	// ASSOCIATIONS
+	// ------------------------
 
-    //------------------------
-    // ASSOCIATIONS
-    //------------------------
+	@ManyToMany(mappedBy = "accounts", fetch = FetchType.EAGER)
+	private Set<Match> matches;
 
-    @LazyCollection(LazyCollectionOption.FALSE)
-    @OneToMany(mappedBy = "account")
-    private List<Match> accountMatches;
+	// ------------------------
+	// CONSTRUCTOR
+	// ------------------------
 
+	public Account() {
+		name = null;
+		email = null;
+		password = null;
+		isVerified = false;
+		isLoggedIn = false;
+		score = 0;
+		level = 0;
+		matches = new HashSet<Match>();
+	}
 
-    //------------------------
-    // CONSTRUCTOR
-    //------------------------
+	public Account(String aName, String aEmail, String aPassword, boolean verified, boolean loggedIn, int aScore,
+			int aLevel) {
+		name = aName;
+		email = aEmail;
+		password = aPassword;
+		isVerified = verified;
+		isLoggedIn = loggedIn;
+		score = aScore;
+		level = aLevel;
+		matches = new HashSet<Match>();
+	}
 
-    public Account() {
-        name = null;
-        email = null;
-        password = null;
-        isVerified = false;
-        isLoggedIn = false;
-        score = 0;
-        level = 0;
-        accountMatches = new ArrayList<>();
-    }
+	// ------------------------
+	// INTERFACE
+	// ------------------------
 
-    public Account(String aName, String aEmail, String aPassword, boolean verified, boolean loggedIn, int aScore, int aLevel) {
-        name = aName;
-        email = aEmail;
-        password = aPassword;
-        isVerified = verified;
-        isLoggedIn = loggedIn;
-        score = aScore;
-        level = aLevel;
-        accountMatches = new ArrayList<>();
-    }
+	public String getName() {
+		return name;
+	}
 
+	public String getEmail() {
+		return email;
+	}
 
-    //------------------------
-    // INTERFACE
-    //------------------------
+	public String getPassword() {
+		return password;
+	}
 
-    public String getName() { return name; }
+	public boolean isLoggedIn() {
+		return isLoggedIn;
+	}
 
-    public String getEmail() { return email; }
+	public boolean isVerified() {
+		return isVerified;
+	}
 
-    public String getPassword() { return password; }
+	public int getScore() {
+		return score;
+	}
 
-    public boolean isLoggedIn() { return isLoggedIn; }
+	public int getLevel() {
+		return level;
+	}
 
-    public boolean isVerified() { return isVerified; }
+	public Set<Match> getMatches() {
+		return matches;
+	}
 
-    public int getScore() { return score; }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getLevel() { return level; }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public List<Match> getAccountMatches() { return accountMatches; }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
+	public void setLoggedIn(boolean loggedIn) {
+		isLoggedIn = loggedIn;
+	}
 
-    public void setName(String name) { this.name = name; }
+	public void setVerified(boolean verified) {
+		isVerified = verified;
+	}
 
-    public void setEmail(String email) { this.email = email; }
+	public void setScore(int score) {
+		this.score = score;
+	}
 
-    public void setPassword(String password) { this.password = password; }
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	public void setMatches(Set<Match> matches) {
+		this.matches = matches;
+	}
 
-    public void setLoggedIn(boolean loggedIn) { isLoggedIn = loggedIn; }
+	public void addMatches(HashSet<Match> matches) {
+		for (Match match : matches) {
+			addMatch(match);
+		}
+	}
 
-    public void setVerified(boolean verified) { isVerified = verified; }
+	public void addMatch(Match match) {
+		this.matches.add(match);
+		if(match.getAccounts().isEmpty() || !match.getAccounts().contains(this)){
+			match.addAccount(this);
+		}
+	}
 
-    public void setScore(int score) { this.score = score; }
+	public void removeMatch(Match match) {
+		this.matches.remove(match);
+		if(match.getAccounts().contains(this)){
+			match.removeAccount(this);
+		}
+	}
 
-    public void setLevel(int level) { this.level = level; }
-
-    public void setAccountMatches(List<Match> accountMatches) { this.accountMatches = accountMatches; }
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Account && ((Account) o).email.equals(this.email)) {
+			return true;
+		}
+		return false;
+	}
 }
-
