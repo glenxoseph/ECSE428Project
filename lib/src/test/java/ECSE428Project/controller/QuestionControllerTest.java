a935e270af9b2343acf645b9e90e3f0de113e647
Glen <jiayuan.xu@mail.mcgill.ca>
2021-11-28 10:51:49 -0500

    integration test 2

--- lib/src/test/java/ECSE428Project/controller/QuestionControllerTest.java
+++ lib/src/test/java/ECSE428Project/controller/QuestionControllerTest.java
@@ -11,11 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @Tag("IntegrationTest")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -68,4 +70,54 @@ public class QuestionControllerTest {
                 .andExpect(content().json("[\"Post-Renaissance European Art\"]"));
 
     }
+
+    @Test
+    public void testCorrectAnswer() throws Exception {
+        Account account = TestUtilities.createAccount(1);
+        AccountCreateDto accountCreateDto = new AccountCreateDto();
+        accountCreateDto.setName(account.getName());
+        accountCreateDto.setEmail(account.getEmail());
+        accountCreateDto.setPassword(account.getPassword());
+
+        String quizId = "null";
+        String quizName = "Art";
+        String quizTopic = "ART";
+        String question = "Which artist pioneered the Italian Renaissance?";
+        String choiceOne = "Picasso";
+        String choiceTwo = "Da Vinci";
+        String choiceThree = "Botticelli";
+        String choiceFour = "Titan";
+
+        // Create an account and post it to the database
+        mockMvc.perform(post("/createAccount").content("{\n" +
+                                "\"name\": \"" + accountCreateDto.getName() + "\",\n" +
+                                "\"email\": \"" + accountCreateDto.getEmail() + "\",\n" +
+                                "\"password\": \"" + accountCreateDto.getPassword() + "\"\n" +
+                                "}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/quiz/create").content("[{\"id\":" + quizId
+                                + ",\"name\":\"" + quizName
+                                + "\",\"topic\":\"" + quizTopic
+                                + "\",\"questions\":[{\"id\":null,\"askedQuestion\":\"" + question
+                                + "\", \"possibleAnswers\":[\"" + choiceOne
+                                + "\",\"" + choiceTwo + "\",\"" + choiceThree
+                                + "\",\"" + choiceFour + "\"],\"answer\":\"" + choiceOne + "\"}]}]")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        MvcResult result = mockMvc.perform(get("/quiz/" + quizName + "/questions").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.[*].question").value(question))
+                .andExpect(jsonPath("$.[*].answer").value(choiceOne))
+                .andExpect(jsonPath("$.[*].possibleAnswers[0]").value(choiceOne))
+                .andExpect(jsonPath("$.[*].possibleAnswers[1]").value(choiceTwo))
+                .andExpect(jsonPath("$.[*].possibleAnswers[2]").value(choiceThree))
+                .andExpect(jsonPath("$.[*].possibleAnswers[3]").value(choiceFour))
+                .andReturn();
+
+    }
+
 }
