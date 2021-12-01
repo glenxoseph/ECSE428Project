86bf3b80ec1e3af5840dceb1d9bc3dd09c33ef2b
MaxensDestine <38329417+maxensdestine@users.noreply.github.com>
2021-12-01 13:27:46 -0500

    Fixed the login
    The login will now fail if  the account is banned

--- lib/src/main/java/ECSE428Project/service/LoginService.java
+++ lib/src/main/java/ECSE428Project/service/LoginService.java
@@ -15,6 +15,9 @@ public class LoginService {
 
     @Autowired
     private AccountRepository accountRepository;
+    
+    @Autowired
+    private AdminConfigService adminService;
 
     @Transactional
     public Account profileLogin(String email, String password) throws ResponseStatusException {
@@ -36,8 +39,9 @@ public class LoginService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No account associated to the provided credentials");
         }
 
+        boolean isBanned = adminService.getBannedEmails().contains(profile.getEmail());
         // Checking if password is correct and account isn't logged in
-        if (password.equals(profile.getPassword()) && !profile.isLoggedIn()) {
+        if (password.equals(profile.getPassword()) && !profile.isLoggedIn() && !isBanned) {
             profile.setLoggedIn(true);
             profile = accountRepository.save(profile);
             return profile;
@@ -46,6 +50,8 @@ public class LoginService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Already Logged In");
         } else if (!password.equals(profile.getPassword())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password did not match records");
+        } else if(isBanned) {
+        	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The email associated to this account is banned");
         }
         else {
             return null;
